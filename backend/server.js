e4872d5c9b3586ec67f1461057dc8edf3da14054
Henil Suhagiya <henilsuhagiya0@gmail.com>
2025-01-17 13:39:50 +0530

    added vercel.json file and configured cors policy

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,13 @@ connectDB();
 
 const app = express();
 app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
+
 app.use(express.json());
 
 // Routes
